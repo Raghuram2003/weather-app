4965bdd846849bc36ddf0bcca773553fb7932dd3
Raghuram2003 <destrobroxd@gmail.com>
2023-09-09 00:40:26 +0530

    forecast changes

---
+++ src/api.js
@@ -0,0 +1,12 @@
+export const geoApiOptions = {
+  method: "GET",
+  headers: {
+    "X-RapidAPI-Key": "a4ae61149bmsheeffd801c97c76dp18ff9bjsn414603976dee",
+    "X-RapidAPI-Host": "wft-geo-db.p.rapidapi.com",
+  },
+};
+
+export const geoApiUrl = "https://wft-geo-db.p.rapidapi.com/v1/geo";
+
+export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5";
+export const WEATHER_API_KEY = "524b1de9ef7d5ebb6bd666a65ce7a54b";
